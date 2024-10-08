2473710ca04288d4ff814ef4df9426bd8801440b
Jaakko Saarela <jaakko.saarela@parcnu.fi>
2024-10-08 10:05:52 +0300

    Changed userapp to run backgound. killing userapp by sending SIGTERM. output.log added to .gitignore

--- README.md
+++ README.md
@@ -4,24 +4,57 @@
 ## Wiring of RPI4B to mm5d91
 ![wiring](images/image.png)
 ## Prerequisite
-* sudo apt install raspberrypi-kernel-headers
-* sudo cp mm5d91_overlay.dtbo /boot/overlays
-* sudo vi /boot/config.txt
-    * add "dtoverlay=mm5d91_overlay"
-* sudo raspi-config
-    * interfaces->serial->login shell to be accessible over serial -> NO
-    * interfaces->serial->Serial port hardware enabled -> YES
-    * reboot raspi
-## Install to kernel
-* sudo insmod mm5d91_driver.ko
-## Remove from kernel
-* sudo rmmod mm5d91_driver
-## Compile
-* make
-## Userapp compile
-* gcc usertestapp.c -o usertestapp
+```
+sudo apt install raspberrypi-kernel-headers
+sudo cp mm5d91_overlay.dtbo /boot/overlays
+```
+### add "dtoverlay=mm5d91_overlay" to /boot/config.txt
+```
+sudo vi /boot/config.txt
+````
+### Run raspi-config
+```
+sudo raspi-config
+```
+### Change following to raspi-config:
+```
+    interfaces->serial->login shell to be accessible over serial -> NO
+    interfaces->serial->Serial port hardware enabled -> YES
+    reboot raspi
+```
+## Install driver to kernel
+```
+sudo insmod mm5d91_driver.ko
+```
+## Remove driver from kernel
+```
+sudo rmmod mm5d91_driver
+```
+## Compile driver
+```
+make
+```
+# Userapp 
+## Compile userapp
+```
+gcc usertestapp.c -o usertestapp -lpthread
+```
 ## Run user app
-* ./usertestapp
+```
+./usertestapp &
+```
+* output.log file is created in the same folder. Each time app is started output.log will be over written.
+
+### show the 50 last lines of the output.log:
+```
+tail -n 50 output.log 
+```
+
+## Kill the running app
+```
+kill -SIGTERM <pid>
+```
+* pid is printed out when app is starting up.  
 ## Message path from kernel to user app.
 ![MSG](images/msg_path.png)
 ## TODO
--- usertestapp.c
+++ usertestapp.c
@@ -4,6 +4,7 @@
 #include <sys/ioctl.h>
 #include <signal.h>
 #include <unistd.h>
+#include <string.h>
 #include "mm5d91_ioctl.h"
 
 unsigned int userbuffer[BUFFER_LENGTH];
@@ -11,35 +12,80 @@ int fd;
 unsigned char user_buf[BUFFER_LENGTH];
 int len;
 static int calc = 0;
+volatile sig_atomic_t running = 1;  // To stop loop when SIGTERM is received
 
+// Signal handler for kernel signals (SIGUSR1)
 void signal_handler(int sig)
 {
-	int32_t cnt;
-	calc++;
-	printf("Signal Received %i\n", calc);
-	cnt = ioctl(fd, IOCTL_GET_MSG_LEN, (int32_t *) &cnt);
-	printf("CNT %i\n", cnt);
-	read(fd,user_buf,cnt);
-	user_buf[10] = '\0';
-	for (int i = 0 ; i<cnt; i++)
-		printf("i %i, MSG: 0x%02x\n",i, user_buf[i]);
+    int32_t cnt;
+    calc++;
+    printf("Signal Received %i\n", calc);
+    cnt = ioctl(fd, IOCTL_GET_MSG_LEN, (int32_t *) &cnt);
+    printf("CNT %i\n", cnt);
+    read(fd, user_buf, cnt);
+    user_buf[cnt] = '\0';
+    for (int i = 0; i < cnt; i++)
+        printf("%d\n", user_buf[i]);
 
+    fflush(stdout);  // Flush stdout after printing messages
+}
+
+// Signal handler for SIGTERM (to exit program)
+void sigterm_handler(int sig)
+{
+    printf("SIGTERM received, exiting...\n");
+    fflush(stdout);  // Flush stdout after printing exit message
+    running = 0;
 }
 
 int main(int argc, char *argv[])
 {
-	signal(SIGUSR1, signal_handler);
-	fd = open("/dev/mm5d91", O_RDWR);
-	if (fd < 0) {
-		perror("fd failed");
-		exit(2);
-	}
+    // Fork to run as a background process
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("Fork failed");
+        exit(EXIT_FAILURE);
+    }
 
-	//ioctl(fd, IOCTL_SET_PID_SIG, &p_s);
-	getchar();
-	ioctl(fd, IOCTL_SEND_SIGNAL);
-	perror("ioctl");
-	getchar();
+    // Parent process exits, child runs in background
+    if (pid > 0) {
+        printf("Running as background process. PID: %d\n", pid);
+        fflush(stdout);  // Flush stdout before parent exits
+        exit(EXIT_SUCCESS);
+    }
 
-	close(fd);
+    // Redirect output to a file
+    freopen("output.log", "w", stdout);
+    freopen("output.log", "w", stderr);
+
+    // Signal handler setup
+    signal(SIGUSR1, signal_handler);  // Handle kernel signals
+    signal(SIGTERM, sigterm_handler); // Handle SIGTERM to exit
+    signal(SIGINT, SIG_IGN);          // Ignore Ctrl+C (SIGINT)
+
+    // Open the mm5d91 device file
+    fd = open("/dev/mm5d91", O_RDWR);
+    if (fd < 0) {
+        perror("fd failed");
+        close(fd);
+        exit(2);
+    }
+
+    // Main loop to handle signals until SIGTERM is received
+    while (running) {
+        pause();  // Wait for signals
+    }
+
+    // Flush any remaining output
+    fflush(stdout);
+    fflush(stderr);
+
+    // Close the device file
+    close(fd);
+
+    // Close the redirected streams
+    fclose(stdout);
+    fclose(stderr);
+
+    return 0;
 }
